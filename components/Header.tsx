cd754d2f61022d5e3d5a8d177889dab30954ff35
adrian <adrian@Linux-Cidwa.myguest.virtualbox.org>
2024-04-16 15:55:41 -0600

    16 de abril

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+        <header className='d-flex '>
+            <img src="https://static.posters.cz/image/hp/106363.jpg" alt="" width="3000" height="auto"/>
+            {/* <h1>Menu</h1> */}
+            {/* <h1>Peliculas</h1> */}
+        </header>
+    </div>
+  )
+}
